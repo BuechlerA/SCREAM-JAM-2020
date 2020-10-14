19c15af7c2f16349b878d3ce6557fba4b3b83039
Alexander Büchler <alexander.buechler25@gmail.com>
2020-10-14 15:10:20 +0200

    character controls and camera look

--- Prototype/Scripts/CameraBehaviour.gd
+++ Prototype/Scripts/CameraBehaviour.gd
@@ -1,16 +1,27 @@
 extends Spatial
 
+export var follow_distance = 4.0
+export var follow_height = 2.0
+export var follow_enabled = false
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var player
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	player = get_node("../CharacterController")
+	set_physics_process(true)
+	set_as_toplevel(true)
+	
+func _physics_process(delta):
+	var target = player.get_global_transform().origin
+	var curr_pos = get_global_transform().origin
+	var up_dir = Vector3(0,1,0)
+	if follow_enabled:
+		var offset = curr_pos - target
+		offset = offset.normalized() * follow_distance
+		offset.y  = follow_height
+		
+		curr_pos = target + offset
+		
+		look_at_from_position(curr_pos,target, up_dir)
+	elif !follow_enabled:
+		look_at(target, up_dir)
--- Prototype/Scripts/CharacterController.gd
+++ Prototype/Scripts/CharacterController.gd
@@ -1,24 +1,60 @@
 extends KinematicBody
 
-var gravity = Vector3.DOWN * 12
-var moveSpeed = 4
-var jumpSpeed = 6
-
+var gravity = -10.0
 var velocity = Vector3()
+var camera
+var character
+
+var speed = 4
+var acceleration = 3
+var deacceleration = 5
+var jumpSpeed = 6
+var isJumping = false
+
+func _ready():
+	camera = get_node("../Camera").get_global_transform()
+	character = get_node(".")
 
 func _physics_process(delta):
-	velocity += gravity * delta
-	get_input()
-	velocity = move_and_slide(velocity, Vector3.UP)
+	var dir =  Vector3()
+	var is_moving = false
 	
-func get_input():
-	velocity.x = 0
-	velocity.z = 0
 	if Input.is_action_pressed("move_left"):
-		velocity.x += moveSpeed
+		dir += -camera.basis[0]
+		is_moving = true
 	if Input.is_action_pressed("move_right"):
-		velocity.x -= moveSpeed
-	if Input.is_action_pressed("move_up"):
-		velocity.z += moveSpeed
-	if Input.is_action_pressed("move_down"):
-		velocity.z -= moveSpeed
+		dir += camera.basis[0]
+		is_moving = true
+	if Input.is_action_pressed("move_forward"):
+		dir += -camera.basis[2]
+		is_moving = true
+	if Input.is_action_pressed("move_backward"):
+		dir += camera.basis[2]
+		is_moving = true
+	
+	dir.y = 0
+	dir = dir.normalized()
+	
+	velocity.y += gravity * delta
+	
+	var hv = velocity
+	hv.y = 0 
+	
+	var new_pos = dir * speed
+	var accel = deacceleration
+	
+	if(dir.dot(hv) > 0):
+		accel = acceleration
+	
+	hv = hv.linear_interpolate(new_pos, accel * delta)
+	
+	velocity.x = hv.x
+	velocity.z = hv.z
+	
+	velocity = move_and_slide(velocity, Vector3(0,1,0))
+	
+	if is_moving:
+		var angle = atan2(hv.x, hv.z)
+		var char_rot = character.get_rotation()
+		char_rot.y = angle
+		character.set_rotation(char_rot)
